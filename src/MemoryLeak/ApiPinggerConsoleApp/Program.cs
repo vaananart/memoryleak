6e0bf7554216adb360458dc2474175defa9cdc05
vaananart <vaananart@gmail.com>
2021-03-28 11:38:54 +1100

    adding console app to run on the side when the web app to running

---
+++ src/MemoryLeak/ApiPinggerConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiPinggerConsoleApp
+{
+	class Program
+	{
+		private static HttpClient client = new HttpClient();
+		public static async Task Main(string[] args)
+		{
+			var limit = int.Parse(args[0]);
+			for (int i = 0; i < limit; i++)
+			{
+				var result = await client.GetAsync("https://localhost:44332/api/bigstring");
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
